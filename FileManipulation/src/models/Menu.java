be4b9a3dfcaf273cc0a09bdb8168d9e2e735819a
Rafael Augusto <rafaelaugustofsilva@gmail.com>
2024-04-05 15:53:48 -0300

    Menu

---
+++ FileManipulation/src/models/Menu.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Menu {
+	public static void menu() {
+		System.out.println("1 - Criar novo produto");
+		System.out.println("2 - Criar novo eletrônico");
+		System.out.println("3 - Criar comida");
+		System.out.println("4 - Ler produto");
+		System.out.println("9 - Sair");
+	}
+}
